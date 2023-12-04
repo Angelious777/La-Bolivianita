45ce2a85404b89dcff23a55dfbede3fd71c6b825
Angel_Escobar <angel.daniel.escobar77@gmail.com>
2023-12-04 19:40:44 -0400

    mejorated all

--- js.js
+++ js.js
@@ -1,7 +1,25 @@
+// function mostrar_cuadro_registros(){
+//     document.getElementById("registro").style.display = "flex";
+// }
+
+// function cerrar_registro(){
+//     document.getElementById("registro").style.display = "none";
+// }
+
 function mostrar_cuadro_registros(){
-    document.getElementById("registro").style.display = "flex";
+    var registro = document.getElementById("registro");
+    if (registro.classList.contains("registro")){
+        registro.classList.remove("registro");
+        registro.classList.add("registro_on")
+    }
+    else{
+        registro.classList.remove("registro_on");
+        registro.classList.add("registro")
+    }
 }
 
 function cerrar_registro(){
-    document.getElementById("registro").style.display = "none";
+    var registro = document.getElementById("registro");
+    registro.classList.remove("registro_on");
+    registro.classList.add("registro")
 }
