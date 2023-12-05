372c50dfaaafaac3d4e428f4f818f859f3a0a91b
Angel_Escobar <angel.daniel.escobar77@gmail.com>
2023-12-05 03:35:52 -0400

    added new functions in index.html

--- js.js
+++ js.js
@@ -23,3 +23,38 @@ function cerrar_registro(){
     registro.classList.remove("registro_on");
     registro.classList.add("registro")
 }
+
+
+
+
+function mas_popular(){
+    document.getElementById("cuadro_populares").style.display ="flex";
+    document.getElementById("cuadro_ultima").style.display ="none";
+    document.getElementById("cuadro_clasico").style.display ="none";
+
+    document.getElementById("popular").style.backgroundColor ="dimgray";
+    document.getElementById("ultima").style.backgroundColor ="transparent";
+    document.getElementById("clasico").style.backgroundColor ="transparent";
+}
+
+function ultima_pub(){
+    document.getElementById("cuadro_populares").style.display ="none";
+    document.getElementById("cuadro_ultima").style.display ="flex";
+    document.getElementById("cuadro_clasico").style.display ="none";
+
+    document.getElementById("popular").style.backgroundColor ="transparent";
+    document.getElementById("ultima").style.backgroundColor ="dimgray";
+    document.getElementById("clasico").style.backgroundColor ="transparent";
+
+}
+
+function los_clasicos(){
+    document.getElementById("cuadro_populares").style.display ="none";
+    document.getElementById("cuadro_ultima").style.display ="none";
+    document.getElementById("cuadro_clasico").style.display ="flex";
+
+    document.getElementById("popular").style.backgroundColor ="transparent";
+    document.getElementById("ultima").style.backgroundColor ="transparent";
+    document.getElementById("clasico").style.backgroundColor ="dimgray";
+
+}
